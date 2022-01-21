4481ae462763d8b4f41ff909c1b4f3b183d9f9bd
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-01-21 12:35:52 -0800

    switching roles

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const sequelize = require('sequelize');
 const morgan = require('morgan');
+const layout = require('./views/layout');
+const { db, Page, User } = require('./models');
+
 
 const app = express();
+const PORT = 1337;
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+app.get('/', (req, res, next) => {
+    res.send(layout(''));
+})
+
+let sync = async function(){
+    await db.sync({ force: true });
+    app.listen(PORT, () => {
+        console.log('listening')
+    })
+}
+
+sync();
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
